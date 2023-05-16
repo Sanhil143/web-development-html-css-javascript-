fc5b916e85ebd9e52555577eb245efd9fd61d5b5
Sanhil143 <sanhilrai143@gmail.com>
2023-05-17 00:36:30 +0530

    Promise

--- Javascript/4-promises/promises.js
+++ Javascript/4-promises/promises.js
@@ -1,18 +1,18 @@
 
-
-const ticket = new Promise((resolve,reject) => {
-      const isBooked = false;
-      if(isBooked){
+//normal syntax of promise
+const ticket = new Promise((resolve, reject) => {
+      const isBooked = true;
+      if (isBooked) {
             resolve('Ticket is booked');
-      }else{
+      } else {
             reject('Ticket is cancelled');
       }
 })
 
-
 ticket
-.then((data) => {
-      console.log("wohoo",data);
-}).catch((data) => {
-      console.log("oops",data);
-})
+      .then((data) => {
+            console.log("wohoo", data);
+      }).catch((data) => {
+            console.log("oops", data);
+      })
+
