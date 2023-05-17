f4fbae2cf94e3cd5c34e0e19389a48e636a0cc81
Sanhil143 <sanhilrai143@gmail.com>
2023-05-18 00:27:41 +0530

    applied js on html

--- Javascript/Dom/script.js
+++ Javascript/Dom/script.js
@@ -1,3 +1,12 @@
 
+const container = document.getElementsByClassName('container')[0];
+container.style.backgroundColor = 'red';
 
-const boxes = document.getElementsByClassName('box')
+
+const box = document.getElementsByClassName('box');
+
+for(let i=0; i<box.length; i++){
+      let boxes = box[i]
+      boxes.style.borderRadius = '20%'
+}
+
